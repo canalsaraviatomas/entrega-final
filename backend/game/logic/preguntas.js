64cef9a500cf4ef0390e508d4930b2d152873ae8
Tomas Canal <tomascanalsaravia@gmail.com>
2025-07-16 12:05:24 -0300

    Paso 4

--- backend/game/logic/preguntas.js
+++ backend/game/logic/preguntas.js
@@ -1,1 +1,23 @@
-// TODO: implementar
+const fs = require("fs");
+const path = require("path");
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function obtenerPreguntas() {
+  const preguntasPath = path.join(
+    __dirname,
+    "../../data/preguntas/Preguntas.json"
+  );
+  const data = fs.readFileSync(preguntasPath, "utf8");
+  let preguntas = JSON.parse(data);
+  preguntas = shuffle(preguntas);
+  return preguntas;
+}
+
+module.exports = { obtenerPreguntas };
--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const socketIO = require('socket.io');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketIO = require("socket.io");
 
 const app = express();
 
 // Servir archivos estáticos
-app.use('/css', express.static(path.join(__dirname, '../frontend/css')));
-app.use('/js', express.static(path.join(__dirname, '../frontend/js')));
-app.use('/views', express.static(path.join(__dirname, '../frontend/views')));
+app.use("/css", express.static(path.join(__dirname, "../frontend/css")));
+app.use("/js", express.static(path.join(__dirname, "../frontend/js")));
+app.use("/views", express.static(path.join(__dirname, "../frontend/views")));
 
 // Ruta raíz sirve index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/views/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/views/index.html"));
 });
 
 const server = http.createServer(app);
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`🟢 Cliente conectado: ${socket.id}`);
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`🔴 Cliente desconectado: ${socket.id}`);
   });
 });
